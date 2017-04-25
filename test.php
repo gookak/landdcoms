0305f84b799bc83a6b5d96eb6d75c9a7cdf2c128
songkitsubarruk <songkitsubarruk@gmail.com>
2017-04-25 09:34:41 +0700

    Revert "ttt"
    This reverts commit 6788ca0453cbdcfc954e66643afb1c6c9826e22d.

--- test.php
+++ test.php
@@ -7,7 +7,7 @@
 		echo '<p>Hello World</p>'; 
 		echo '<p>Hello World 222</p>'; 
 		echo '<p>Hello World 333</p>'; 
-		echo '<p>Hello World by Boy123456789</p>'; 
+		echo '<p>Hello World by Boy</p>'; 
 	?> 
 	
  </body>
