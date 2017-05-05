82dfb160daccd7a70a3551d04a7db4c2f95e4667
songkitsubarruk <songkitsubarruk@gmail.com>
2017-05-05 17:05:10 +0700

    Product Serarch
    Product Search by Name Category Price Min & Max
    SideBar Menu Category Responsive

--- frontend/resources/views/product/index.blade.php
+++ frontend/resources/views/product/index.blade.php
@@ -15,13 +15,20 @@
 
 
 <div class="single-product-area">
-    <div class="zigzag-bottom"></div>
     <div class="container">
         <div class="row">
             <div class="col-md-3">
                <div class="single-sidebar">
-                    <h2 class="sidebar-title">Search Products</h2>
-                    <form action="/product" method="GET">
+                    <div class="sub-menu-title">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".search-collapse">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        Search Products
+                    </div>
+                    <form action="/product" class="search-collapse collapse" method="GET">
                         {{-- {{ csrf_field() }} --}}
                         <p class="form-row">
                             <label for="name">ชื่อสินค้า</label>
@@ -31,7 +38,7 @@
                             <label for="category_id">ประเภทสินค้า</label>
                             <select id="category_id" name="category_id" class="form-control">
                                 <option value="">เลือกประเภท...</option>
-                                @foreach($categorys as $category)
+                                @foreach($category_list as $category)
                                 <option value="{{$category->id}}">{{$category->name}}</option>
                                 @endforeach
                             </select>
@@ -46,24 +53,29 @@
                     </form>
                 </div>
 
-                {{-- <div class="single-sidebar">
-                    <h2 class="sidebar-title">ประเภทสินค้า</h2>
-                    <div class="list-group">
-                        @foreach($categorys as $category)
-                        <a href="/product?category={{$category->id}}" class="list-group-item">{{$category->name}}</a>
+                <div class="single-sidebar">
+                    <div class="sub-menu-title">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".list-group-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        ประเภทสินค้า
+                    </div>
+                   
+                    <div class="list-group list-group-collapse collapse">
+                        @foreach($category_list as $category)
+                        <a href="/product?category_id={{$category->id}}" class="list-group-item">{{$category->name}}</a>
                         @endforeach
                     </div>
-                </div> --}}
-                   {{--  <div class="panel panel-primary">
-                    <div class="panel-heading product-big-title-area">ประเภทสินค้า</div>
-                    <div class="list-group">
-                      <a href="#" class="list-group-item active">Smartphone</a>
-                      <a href="#" class="list-group-item">Notebook</a>
-                      <a href="#" class="list-group-item">Software</a>
-                  </div>
-              </div> --}}
+                </div>
             </div>
             <div class="col-md-9">
+                <div class="product-breadcroumb">
+                    <a href="/">Home</a>
+                    <a href="/product?category_id={{$category_current->id}}">{{$category_current->name}}</a>
+                </div>
                 @if ($products->count())
                     @foreach($products as $product)
                         <div class="col-md-3 col-sm-6">
